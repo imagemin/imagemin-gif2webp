cedeba47f412bced32e0143b4e68778724040253
Shogo Sensui <shogosensui@gmail.com>
2020-05-30 14:40:43 +0900

    Require Node.js 10 (#3)

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import isWebP from 'is-webp';
-import pify from 'pify';
-import test from 'ava';
-import m from '.';
+const {promisify} = require('util');
+const fs = require('fs');
+const path = require('path');
+const isWebP = require('is-webp');
+const test = require('ava');
+const m = require('.');
 
-const fsP = pify(fs);
+const readFile = promisify(fs.readFile);
 
 test('convert an image into a WebP', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures/test.gif'));
+	const buf = await readFile(path.join(__dirname, 'fixtures/test.gif'));
 	const data = await m()(buf);
 
 	t.true(data.length < buf.length);
@@ -16,7 +16,7 @@ test('convert an image into a WebP', async t => {
 });
 
 test('skip optimizing unsupported files', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures/test.bmp'));
+	const buf = await readFile(path.join(__dirname, 'fixtures/test.bmp'));
 	const data = await m()(buf);
 
 	t.deepEqual(data, buf);
